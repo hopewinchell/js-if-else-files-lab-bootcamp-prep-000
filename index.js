ba23152dde940c7452d5a0b691cd1718026994c3
Hope Winchell <hopewinchell@gmail.com>
2018-05-30 15:26:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var height = "74"
 if (name === "Susan") {
   console.log("This name is right.")
 } else {
-  ("The name was supposed to be Susan.")
+  console.log("The name was supposed to be Susan.")
 }
 // Don't worry about this
 module.exports = { name, height
